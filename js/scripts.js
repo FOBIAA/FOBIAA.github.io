465c4e65dd7830be61cb7f4e6c4d1f7534c24a1f
Omar Einea <eineao@gmail.com>
2016-04-02 13:44:26 +0300

    Bring OCD and FOBIAA demo logo

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,18 @@ $(document).ready(function() {
     
     $("#summary a").click(function() {
         $("#summary").addClass("nbd-yellow");
+        $("#nbd").show();
+        
         $("#donate").removeClass("nbd-yellow");
+        $("#ocd").hide();
     });
     
     $("#donate a").click(function() {
         $("#donate").addClass("nbd-yellow");
+        $("#ocd").show();
+        
         $("#summary").removeClass("nbd-yellow");
+        $("#nbd").hide();
     });
     
     $("#summary a").trigger('click');
